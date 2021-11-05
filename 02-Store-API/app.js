b2128bc95a981f2bdecbd0ef70f2c77ecdb2f9ba
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-05 10:48:17 +0100

    model done

--- 02-Store-API/app.js
+++ 02-Store-API/app.js
@@ -1,11 +1,17 @@
+// import dotenv and async-errors package
 require('dotenv').config();
-// async errors
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
+// import DB
 const connectDB = require('./db/connect');
 
+// import controllers
+const productsRouter = require('./routes/products');
+
+// errors middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -17,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
-// products route
+app.use('/api/v1/products', productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
