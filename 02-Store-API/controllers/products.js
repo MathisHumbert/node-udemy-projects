cfbf39f82f2cdefd9948fcaa34fca0b7e287cad1
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-05 11:44:24 +0100

    find node

--- 02-Store-API/controllers/products.js
+++ 02-Store-API/controllers/products.js
@@ -1,10 +1,35 @@
+// get the model
+const { Query } = require('mongoose');
+const Products = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' });
+  const search = 'a';
+  const products = await Products.find({
+    name: { $regex: search, $options: 'i' },
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  // refractor for mongoose v5
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  console.log(queryObject);
+  const products = await Products.find(queryObject);
+
+  // mongoose V6 way:
+  //const products = await Products.find(req.query);
+
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
