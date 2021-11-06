bda506892d783a65947a6287c28b36e1d9e33e92
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-06 10:56:12 +0100

    refactor code

--- 03-JWT-Basics/controllers/main-no-refactor.js
+++ 03-JWT-Basics/controllers/main-no-refactor.js
@@ -35,12 +35,27 @@ const login = async (req, res) => {
 // compture ==jwt==> server
 // server ==response==> computer
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
+  // get the auth header
+  const authHeader = req.headers.authorization;
 
-  res.status(200).json({
-    msg: `Hello, ${req.user.username}`,
-    secret: `Here is yout authorized data, your lucky number is ${luckyNumber}`,
-  });
+  // if there is nothing
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401);
+  }
+
+  // get only the token string
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is yout authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401);
+  }
 };
 
 module.exports = { login, dashboard };
--- 03-JWT-Basics/routes/main.js
+++ 03-JWT-Basics/routes/main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { login, dashboard } = require('../controllers/main');
+const authenticationMiddleware = require('../middleware/auth');
 
-router.get('/dashboard', dashboard);
+// first middleware auth and then dashboard
+router.get('/dashboard', authenticationMiddleware, dashboard);
 router.post('/login', login);
 
 module.exports = router;
