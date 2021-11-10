4a029ff433613f391a5c52f453bb411a8d9907c0
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-10 18:03:24 +0100

    uplaod image via postman

--- 05-File-Upload/controllers/productController.js
+++ 05-File-Upload/controllers/productController.js
@@ -1,8 +1,11 @@
 const Product = require('../models/Product');
-const { statusCode } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 const createProduct = async (req, res) => {
-  res.send('create product');
+  console.log(req.body);
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
 };
 const getAllProducts = async (req, res) => {
   res.send('list of products');
--- 05-File-Upload/controllers/uploadsController.js
+++ 05-File-Upload/controllers/uploadsController.js
@@ -1,7 +1,20 @@
-const { statusCode } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
+const path = require('path');
 
 const uploadProductImage = async (req, res) => {
-  res.send('create product');
+  const productImage = req.files.image;
+
+  // create the image path
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+
+  //  move the image to the path created
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `uploads/${productImage.name}` } });
 };
 
 module.exports = {
