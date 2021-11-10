7fa719182263116164c3dcbd7215fd30e985da1c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-10 18:32:18 +0100

    uplaod to our own server done, now cloudinary way

--- 05-File-Upload/controllers/uploadsController.js
+++ 05-File-Upload/controllers/uploadsController.js
@@ -1,8 +1,26 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
+  //check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file Uploaded');
+  }
+
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  // check size
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1KB');
+  }
 
   // create the image path
   const imagePath = path.join(
@@ -12,6 +30,8 @@ const uploadProductImage = async (req, res) => {
 
   //  move the image to the path created
   await productImage.mv(imagePath);
+
+  // return the path to the front end
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `uploads/${productImage.name}` } });
