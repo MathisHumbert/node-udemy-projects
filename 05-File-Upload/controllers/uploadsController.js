fa9174222fb4f1ddcf2791fb8efcf9d4eee5c46c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-10 17:30:27 +0100

    routes, controles and model setup are done

---
+++ 05-File-Upload/controllers/uploadsController.js
@@ -0,0 +1,9 @@
+const { statusCode } = require('http-status-codes');
+
+const uploadProductImage = async (req, res) => {
+  res.send('create product');
+};
+
+module.exports = {
+  uploadProductImage,
+};
