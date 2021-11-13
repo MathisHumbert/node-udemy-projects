0b841eda1a70de7bdf1d14ef314fdc040b873894
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-13 15:59:07 +0100

    project is done

--- 07-Stripe-Payment/public/browser-app.js
+++ 07-Stripe-Payment/public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  'pk_test_51JvNC8FET1oGrBV1aKVo0acXtLVw3SyzePWvG1PaxfbOtzRmmLpvPTCA7WTjcE01lYLnyBFbL5tQ0hy3ArzkgP0B00kvLLdqPU'
 );
 
 // The items the customer wants to buy
