8ddd4a0307148b9a97795aaf5cf00a54177a4d5b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-17 18:30:08 +0100

    cookie env is done

--- 08-E-Commerce-API/controllers/authController.js
+++ 08-E-Commerce-API/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { createJWT } = require('../routes/utils');
+const { attachCookiesToResponse } = require('../routes/utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -19,13 +19,8 @@ const register = async (req, res) => {
   const user = await User.create({ name, email, password, role });
 
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  const token = createJWT({ payload: tokenUser });
 
-  const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie('token', token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-  });
+  attachCookiesToResponse({ res, user: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
