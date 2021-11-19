7cb367028e22cd6b2f1b07510bcbcccc7e2d5794
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-19 15:58:08 +0100

    create review done

--- 08-E-Commerce-API/controllers/reviewController.js
+++ 08-E-Commerce-API/controllers/reviewController.js
@@ -1,5 +1,34 @@
+const Review = require('../models/Review');
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils');
+
 const createReview = async (req, res) => {
-  res.send('create review');
+  console.log(req.body);
+  const productId = req.body.product;
+
+  // check if there is a product
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  // check id the user has already submitted a review
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError(
+      `Already submitted review for this product`
+    );
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
