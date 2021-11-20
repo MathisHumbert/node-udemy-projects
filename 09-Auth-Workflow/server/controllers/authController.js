365790f079ad563470da3fa1d43153d5f1c6b74c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-20 16:14:34 +0100

    token model created

--- 09-Auth-Workflow/server/controllers/authController.js
+++ 09-Auth-Workflow/server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const {
@@ -31,15 +32,12 @@ const register = async (req, res) => {
   });
 
   const origin = 'http://localhost:3000';
-  const protocol = req.protocol;
-  const host = req.get('host');
-  console.log(`protocol: ${protocol}, host: ${host}`);
 
-  const forwardedHost = req.get('x-forwarded-host');
-  const forwardedProtocol = req.get('x-forwarded-proto');
-  console.log(
-    `forwardedprotocol: ${forwardedProtocol}, forwardedhost: ${forwardedHost}`
-  );
+  // const protocol = req.protocol;
+  // const host = req.get('host');
+
+  // const forwardedHost = req.get('x-forwarded-host');
+  // const forwardedProtocol = req.get('x-forwarded-proto');
 
   // send email
   await sendVerificationEmail({
@@ -99,9 +97,21 @@ const login = async (req, res) => {
   }
 
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({ user: tokenUser });
+  // create refresh token
+  let refreshToken = '';
+  // check for existing token
+
+  refreshToken = crypto.randomBytes(40).toString('hex');
+  const userAgent = req.headers['user-agent'];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  const token = await Token.create(userToken);
+
+  // attachCookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser, token });
 };
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
--- 09-Auth-Workflow/server/utils/jwt.js
+++ 09-Auth-Workflow/server/utils/jwt.js
@@ -12,11 +12,12 @@ const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
 
-  const oneDay = 1000 * 60 * 60 * 24;
+  // const oneDay = 1000 * 60 * 60 * 24;
+  const fiveSeconds = 1000 * 5;
 
   res.cookie('token', token, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    expires: new Date(Date.now() + fiveSeconds),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
   });
